03b3a786097966a934303f7e1b1a02487a8f47f4
Shivam Rao <shivamrao8822@gmail.com>
2025-09-12 22:53:05 +0530

    Implemented the Admin Panel

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import RecruiterProfile, FreelancerProfile
 
-# Register your models here.
+@admin.register(RecruiterProfile)
+class RecruiterProfileAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "company_name")
+    search_fields = ("user__email", "company_name")
+
+@admin.register(FreelancerProfile)
+class FreelancerProfileAdmin(admin.ModelAdmin):
+    list_display = ("id", "user")
+    search_fields = ("user__email","skills")
