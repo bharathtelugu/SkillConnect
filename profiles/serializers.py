fbacbc67ef6c3e1e3658cbbbb55c090354e69864
Shivam Rao <shivamrao8822@gmail.com>
2025-09-14 02:40:47 +0530

    added more fields in recruiter and freelancer fields also added them to the frontend, rebase required

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -2,9 +2,19 @@ from rest_framework import serializers
 from .models import RecruiterProfile, FreelancerProfile, Skill
 
 class RecruiterProfileSerializer(serializers.ModelSerializer):
+
+    company_logo_url = serializers.SerializerMethodField()
+
+
     class Meta:
         model = RecruiterProfile
-        fields = ["company_name", "company_website", "location", "about"]
+        fields = ["company_name", "company_website", "location", "about", "industry", "email", "phone", "linkedin_url","company_logo", "company_logo_url"]
+
+    def get_company_logo_url(self,obj):
+        request = self.context.get("request")
+        if obj.company_logo and request:
+            return request.build_absolute_uri(obj.company_logo.url)
+        return None
 
 class SkillSerializer(serializers.ModelSerializer):
     class Meta:
@@ -23,12 +33,14 @@ class FreelancerProfileSerializer(serializers.ModelSerializer):
 
     experience_display = serializers.CharField(source="get_experience_display", read_only=True)
 
+    availability_display = serializers.CharField(source="get_availability_display", read_only=True)
 
     resume_url = serializers.SerializerMethodField()
 
     class Meta:
         model = FreelancerProfile
-        fields = ["name","dob","education", "experience", "experience_display", "skills", "skill_ids", "portfolio_url","resume", "resume_url"]
+        fields = ["name","dob","education", "experience", "experience_display", "skills", "skill_ids", "portfolio_url","resume", "resume_url",
+                  "email", "phone", "location", "github_url", "linkedin_url", "expected_salary", "availability", "availability_display"]
     
     def get_resume_url(self,obj):
         request = self.context.get("request")
